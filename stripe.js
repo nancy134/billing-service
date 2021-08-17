c94a3217463437ef62de680cc9dd8620df7aa466
Nancy Piedra <nancy_piedra@yahoo.com>
2021-08-17 14:34:57 +0000

    Stripe integration

--- stripe.js
+++ stripe.js
@@ -176,28 +176,59 @@ exports.getSetupIntentMe = function(authParams){
 exports.getPaymentMethodMe = function(authParams){
     // 1. Get user by authParams getUserMe
     // 2. Get customer by user.email.  Should have payment method attached
-    console.log("getPaymemtMethodMe");
     return new Promise(function(resolve, reject){
         jwt.verifyToken(authParams).then(function(jwtResult){
+            var cognitoId = jwtResult['cognito:username']; 
             userService.getUserMe(authParams).then(function(user){
-                console.log(user);
                 var customerParams = {
                     email: user.email
                 };
-                exports.listCustomers(customerParams).then(function(customer){
-                    console.log("customer:");
-                    console.log(customer);
-                    var payParams = {
-                        customer: customer.data[0].id,
-                        type: 'card'
-                    };
-                    console.log("payParams:");
-                    console.log(payParams);
-                    exports.getPaymentMethods(payParams).then(function(paymentMethods){
-                        resolve(paymentMethods);
-                    }).catch(function(err){
-                        reject(err);
-                    });
+                exports.listCustomers(customerParams).then(function(customers){
+                    var found = false;
+                    for (var i=0; i<customers.data.length; i++){
+                        if (customers.data[i].metadata.cognitoId === cognitoId){
+                            var found = true;
+                            var payParams = {
+                                customer: customers.data[i].id,
+                                type: 'card'
+                            };
+                            exports.getPaymentMethods(payParams).then(function(paymentMethods){
+                                resolve(paymentMethods);
+                            }).catch(function(err){
+                                reject(err);
+                           });
+                       }
+                    }
+                    if (!found){
+                        var ret = {
+                            statusCode: 400,
+                            message: "customer not found"
+                        };
+                        reject(ret);
+                    } 
+                }).catch(function(err){
+                    reject(err);
+                });
+            }).catch(function(err){
+                reject(err);
+            });
+        }).catch(function(err){
+            reject(err);
+        });
+    });
+}
+
+exports.createCustomerAndIntent = function(authParams, customerBody){
+    return new Promise(function(resolve, reject){
+        exports.createCustomer(customerBody).then(function(customer){
+            exports.createIntent({
+                customer: customer.id
+            }).then(function(intent){
+                var body = {
+                    paymentSecret: intent.client_secret
+                };
+                userService.updateUserMe(authParams, body).then(function(user){
+                    resolve(intent);
                 }).catch(function(err){
                     reject(err);
                 });
@@ -213,44 +244,57 @@ exports.getPaymentMethodMe = function(authParams){
 exports.createPaymentSecret = function(authParams){
     return new Promise(function(resolve, reject){
         jwt.verifyToken(authParams).then(function(jwtResult){
-            // 1. find user via cognitoId       
+            // 1. find user via cognitoId 
+            var cognitoId = jwtResult['cognito:username']; 
             userService.findByCognitoId(jwtResult['cognito:username']).then(function(user){
                 // 2. find customer via user email 
-                exports.listCustomers({email:user.email}).then(function(customer){
+                exports.listCustomers({email:user.email}).then(function(customers){
                     // 3. If customer found, create customer intent
-                    if (customer.data.length > 0){
-                        exports.createIntent({
-                            customer: customer.id
-                        }).then(function(intent){
-                            var body = {
-                                paymentSecret: intent.client_secret
-                            };
-                            userService.updateUserMe(authParams, body).then(function(user){
-                                resolve(intent);
-                            }).catch(function(err){
-                                reject(err);
-                            });
-                        }).catch(function(err){
-                            reject(err);
-                        });
-                    // 4. If customer not found, create customer & intent
-                    } else {
-                        exports.createCustomer({email: user.email}).then(function(customer){
-                            exports.createIntent({
-                                customer: customer.id
-                            }).then(function(intent){
-                                var body = {
-                                    paymentSecret: intent.client_secret
-                                };
-                                userService.updateUserMe(authParams, body).then(function(user){
-                                    resolve(intent);
+                    if (customers.data.length > 0){
+                        var found = false; 
+                        for (var i=0; i<customers.data.length; i++){
+                            if (customers.data[i].metadata.cognitoId === cognitoId){
+                                var found = true;
+                                exports.createIntent({
+                                    customer: customers.data[i].id
+                                }).then(function(intent){
+                                    var body = {
+                                        paymentSecret: intent.client_secret
+                                    };
+                                    userService.updateUserMe(authParams, body).then(function(user){
+                                        resolve(intent);
+                                    }).catch(function(err){
+                                        reject(err);
+                                    });
                                 }).catch(function(err){
                                     reject(err);
                                 });
+                            }
+                        }
+                        // If customer not found, create both customer and intent
+                        if (!found){
+                            var customerBody = {
+                                email: user.email,
+                                metadata: {
+                                    cognitoId: cognitoId
+                                }
+                            };
+                            exports.createCustomerAndIntent(authParams, customerBody).then(function(intent){
                                 resolve(intent);
                             }).catch(function(err){
                                 reject(err);
                             });
+                        }
+                    // 4. If customer not found, create customer & intent
+                    } else {
+                        var customerBody = {
+                            email: user.email,
+                            metadata: {
+                                cognitoId: cognitoId
+                            }
+                        };
+                        exports.createCustomerAndIntent(authParams, customerBody).then(function(intent){
+                            resolve(intent);
                         }).catch(function(err){
                             reject(err);
                         });
