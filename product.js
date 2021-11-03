43f309ad9a78d5fec1f37254d08a234fe679067f
Bill Piedra <Bill@Bills-iMac-770.local>
2021-11-03 10:58:58 -0400

    import products 2

--- product.js
+++ product.js
@@ -34,30 +34,41 @@ exports.getProducts = function(authParams, pageParams, where){
                     limit: pageParams.limit,
                     offset: pageParams.offset
                  }).then(function(result){
-                     resolve(result);
+                     var ret = {
+                         page: pageParams.page,
+                         perPage: pageParams.limit,
+                         products: result
+                     };
+                     resolve(ret);
                  }).catch(function(err){
                      reject(err);
                  });
              } else {
                 reject(utilities.notAuthorized());
             }
-
-
-            
         }).catch(function(err){
             reject(err);
         });
    });
 }
 
-   exports.importProducts = function(authParams, body){
+
+exports.importProducts = function(authParams, body){
     return new Promise(function(resolve, reject){
         jwt.verifyToken(authParams).then(function(jwtResult){
             if (jwt.isAdmin(jwtResult)){
+                var createPromises = [];
                 if (body.products.length){
                     for (var i=0; i<body.products.length; i++){
                         console.log(body.products[i]);
+                        createPromises.push(models.Product.create(body.products[i]));
                     }
+                    Promise.all(createPromises).then(function(values){
+                        resolve(values);
+                    }).catch(function(err){
+                        reject(err);
+                    });
+                    
                 } else {
                     resolve("no products");
                 }
